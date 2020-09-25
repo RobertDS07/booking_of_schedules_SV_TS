e60449298436495781c37849d67942c686769014
Robert Damaceno <robertdamacenobob@gmail.com>
2020-09-25 19:45:22 -0300

    :rocket: add cors and fix somethings

--- src/graphql/controllers/CreateJwt.ts
+++ src/graphql/controllers/CreateJwt.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import {user} from '../../models/User'
 
 const token = (user:user) => {
-    const token = jwt.sign(user, process.env.SECRET || 'hjasdhf873fb312', {
+    const token = jwt.sign({user}, process.env.SECRET || 'hjasdhf873fb312', {
         expiresIn: '365d'
     })
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { graphqlHTTP } from 'express-graphql'
 
 import {schema, resolvers} from './graphql'
 
 const app = express()
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
