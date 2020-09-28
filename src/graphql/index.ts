a06ac02e47b1f5b006d8320e0bcc191012e3048e
Robert Damaceno <robertdamacenobob@gmail.com>
2020-09-28 12:18:27 -0300

    :rocket: transforming in real graphql application k k

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,36 +1,61 @@
 import { buildSchema } from 'graphql'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
-import { Iregister, registerController } from './controllers/register'
-import { Tlogin, loginController } from './controllers/login'
-import { TcheckTimes, checkTimesController } from './controllers/checkTimes'
-import { IMarkHour, markHourController } from './controllers/markHourController'
+import { FlaFlu } from '../models/products/FlaFlu'
+import { PingPong } from '../models/products/PingPong'
+import { User } from '../models/User'
+
+import createToken from './utils/CreateJwt'
+import { Iregister, Tlogin, TcheckTimes, IMarkHour } from './types/types'
 
 export const schema = buildSchema(`
     type Query {
         login(whatsapp:Int!, senha:String!): String
         checkToken(token: String): Boolean
+        checkTimes(produto:String!, dia:String!): [hours]
     }
 
     type Mutation {
         register(nome:String!, whatsapp:Int!, casa:Int!, senha:String!): String
-        checkTimes(produto:String!, dia:String!): [String]
         markHour(produto:String!, horario:String!, token:String!, dia:String!): String
     }
+
+    type hours{
+        hora: String
+    }
+
 `)
 
 export const resolvers = {
-    register: async (args: Iregister) => {
-        const response = await registerController(args)
+    register: async ({ whatsapp, casa, nome, senha }: Iregister) => {
+        if (senha.length < 5) return new Error('Senha deve conter 5 caracteres ou mais')
+        if (casa > 625) return new Error('Casa inválida...')
 
-        return response
-    },
-    login: async (args: Tlogin) => {
-        const response = await loginController(args)
+        const alredyInUse = await User.findOne({ whatsapp })
 
-        return response
+        if (!!alredyInUse) return new Error('Whatsapp já cadastrado')
+
+        const user = await User.create({ senha, nome, casa, whatsapp })
+
+        user.senha = 'undefined'
+
+        const token = createToken(user)
+
+        return token
     },
-    checkToken: ({token}: {token:string}) => {
+    login: async ({ senha, whatsapp }: Tlogin) => {
+        const user = await User.findOne({ whatsapp }).select('+senha')
+
+        if (!user || ! await bcrypt.compare(senha, user.senha)) return new Error('Credenciais invalidas')
+
+        user.senha = 'undefined'
+
+        const token = createToken(user)
+
+        return token
+    },
+    checkToken: ({ token }: { token: string }) => {
         const response = jwt.verify(token, process.env.SECRET || 'hjasdhf873fb312', (err) => {
             if (err) return false
 
@@ -39,14 +64,32 @@ export const resolvers = {
 
         return response
     },
-    checkTimes: async (args: TcheckTimes) => {
-        const response = await checkTimesController(args)
+    checkTimes: async ({ dia, produto }: TcheckTimes) => {
+        let product = FlaFlu
 
-        return response
+        if (produto === 'PingPong') product = PingPong
+
+        const alredyDate = await product.findOne({ dia })
+
+        const res = !!alredyDate ? alredyDate.quemQuando?.filter(e => { if (e.disponivel) return e }) : (await product.create({ dia })).quemQuando?.filter(e => { if (e.disponivel) return e })
+
+        return res
     },
-    markHour: async (args: IMarkHour) => {
-        const response = await markHourController(args)
+    markHour: async ({ dia, token, horario, produto }: IMarkHour) => {
+        let product = FlaFlu
 
-        return response
+        if (produto === 'PingPong') product = PingPong
+
+        const user: any = jwt.verify(token, process.env.SECRET || 'hjasdhf873fb312', (err, decoded) => {
+            if (err) return false
+
+            return decoded
+        })
+
+        if (!user) return new Error('Houve algo errado... Tente fazer login novamente.')
+
+        await product.findOneAndUpdate({ dia, quemQuando: { '$elemMatch': { 'hora': horario } } }, { 'quemQuando.$.disponivel': false, 'quemQuando.$.whatsapp': user.user.whatsapp }, { new: true })
+
+        return 'Succes: Horário marcado com sucesso, qualquer dúvida contatar no whatsapp.'
     }
 }
---
+++ src/graphql/types/types.ts
@@ -0,0 +1,17 @@
+import { IProdutos } from '../../models/products/FlaFlu'
+
+export type Tlogin = Pick<Iregister, 'senha' | 'whatsapp'>
+export type TcheckTimes = Pick<IProdutos, 'produto' | 'dia'>
+
+export interface Iregister {
+    nome:string
+    whatsapp:number
+    senha:string
+    casa:number
+}
+export interface IMarkHour {
+    produto:string
+    horario: string
+    token: string
+    dia: string
+}
